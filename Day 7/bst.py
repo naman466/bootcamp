9844f4ad3adf9463a3bf624299418c018e503584
Naman Tyagi <namantyagi466@gmail.com>
2025-12-15 12:47:31 +0530

    Added balance factor calculations

--- Day 7/bst.py
+++ Day 7/bst.py
@@ -3,6 +3,7 @@ class BSTNode:
         self.val = val
         self.left = None
         self.right = None
+        self.balance_factor = 0  
 
 class BST:
     def __init__(self):
@@ -13,6 +14,7 @@ class BST:
             self.root = BSTNode(val)
         else:
             self._insert(self.root, val)
+        self._update_balance_factors(self.root)
 
     def _insert(self, node, val):
         if val < node.val:
@@ -35,13 +37,41 @@ class BST:
         if node is None:
             return
         self._inorder(node.left, result)
-        result.append(node.val)
+        result.append((node.val, node.balance_factor)) 
         self._inorder(node.right, result)
-        
+
+    def _height(self, node):
+        if node is None:
+            return 0
+        return 1 + max(self._height(node.left), self._height(node.right))
+
+    def _update_balance_factors(self, node):
+        if node is None:
+            return
+        left_height = self._height(node.left)
+        right_height = self._height(node.right)
+        node.balance_factor = left_height - right_height
+        self._update_balance_factors(node.left)
+        self._update_balance_factors(node.right)
+
+    def search(self, val):
+        return self._search(self.root, val)
+
+    def _search(self, node, val):
+        if node is None:
+            return None
+        if val == node.val:
+            return node
+        elif val < node.val:
+            return self._search(node.left, val)
+        else:
+            return self._search(node.right, val)
+
+
 tree = BST()
 arr = [4, 1, 3, 5, 6, 7]
 
 for i in arr:
     tree.insert(i)
 
-print(tree.inorder())
+print(tree.inorder())  
