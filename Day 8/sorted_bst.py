8a1c787164003036491cd0e95d5643669bfcd505
naman466 <namantyagi466@gmail.com>
2025-12-16 15:18:54 +0530

    Solved leetcode 108, binary search tree from sorted array

---
+++ Day 8/sorted_bst.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def helper(low, high):
+            if low > high:
+                return None
+            
+            mid = (low + high) // 2
+            root = TreeNode(nums[mid])
+            
+            root.left = helper(low, mid - 1)
+            root.right = helper(mid + 1, high)
+            
+            return root
+
+        return helper(0, len(nums) - 1)
