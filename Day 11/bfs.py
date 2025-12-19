c78220b4774c3a6050a1de2ab118928d213f8358
naman466 <namantyagi466@gmail.com>
2025-12-19 11:30:23 +0530

    Added BFS in graph

---
+++ Day 11/bfs.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    order = []
+    while queue:
+        node = queue.popleft()
+        order.append(node)
+        for neighbor in graph.get(node, []):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+    return order
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+print(bfs(graph, 'A'))
+
